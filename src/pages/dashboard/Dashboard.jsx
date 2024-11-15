2581004937f2ed57cc5d78ead8bac00672a4fefd
Rk-Dinesh <rdkumar881@gmail.com>
2024-11-15 12:17:51 +0530

    dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,26 +1,127 @@
 import React, { useState } from "react";
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
-import './style.css';
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import "./style.css";
+import assest from '../../assets/assest.png'
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const [selectedValue, setSelectedValue] = useState("");
-  const [value, setValue] = useState()
+  const navigate = useNavigate();
 
-  const handleRadioChange = (event) => {
-    console.log("Selected Value:", event.target.value);
-    setSelectedValue(event.target.value);
-  };
+  const course = [
+    {
+      images: assest,
+      topic: "Topic Name",
+      type: "Text & Video Course",
+      date: "12/12/24",
+      certificate: "no",
+    },
+    {
+      images: assest,
+      topic: "Topic Name",
+      type: "Text & Theory Course",
+      date: "12/11/24",
+      certificate: "no",
+    },
+    {
+      images: assest,
+      topic: "Topic Name",
+      type: "Text & Video Course",
+      date: "12/08/24",
+      certificate: "no",
+    },
+    {
+      images: assest,
+      topic: "Topic Name",
+      type: "Text & Theory Course",
+      date: "12/11/24",
+      certificate: "no",
+    },
+    {
+      images: assest,
+      topic: "Topic Name",
+      type: "Text & Video Course",
+      date: "12/08/24",
+      certificate: "no",
+    },
+  ];
+
+  const redirectcourse = ()=> {
+    navigate('/course')
+  }
 
   return (
-   <div className=" mx-12 my-12">
-    <div className="w-1/2 bg-white px-3 py-1.5">
-    <PhoneInput
-      placeholder="Enter phone "
-      value={value}
-      onChange={setValue}/>
+    <div className=" mx-5 my-6 font-poppins font-extralight">
+      <div className="w-full bg-black">
+        <p className="mx-3 my-5 py-2">
+          Notification Area : We have sent you an verification on your email.
+          Please verify your email to generate by you is incorrect than you can
+          update it from the profile section.
+        </p>
+      </div>
+      <div className="flex gap-4 flex-wrap ">
+        <div className="lg:w-52 md:w-48 w-36 h-56 bg-black my-1">
+          <p className="mx-3 text-normal text-center mt-6">
+            Total Courses Generated{" "}
+          </p>
+          <p className="text-center lg:text-7xl md:text-7xl text-6xl my-4">38</p>
+          <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-2.5 " onClick={redirectcourse}>
+            View
+          </button>
+        </div>
+        <div className="lg:w-52 md:w-48 w-36 h-56 bg-black  my-1">
+          <p className="mx-3 text-normal text-center mt-6">
+          Video Courses Generated{" "}
+          </p>
+          <p className="text-center lg:text-7xl md:text-7xl text-6xl my-4">28</p>
+          <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-2.5 " onClick={redirectcourse}>
+            View
+          </button>
+        </div>
+        <div className="lg:w-52 md:w-48 w-36 h-56 bg-black  my-1">
+          <p className="mx-3 text-normal text-center mt-6">
+          Theory Courses Generated{" "}
+          </p>
+          <p className="text-center lg:text-7xl md:text-7xl text-6xl my-4">18</p>
+          <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-2.5 " onClick={redirectcourse}>
+            View
+          </button>
+        </div>
+        <div className="lg:w-52 md:w-48 w-36 h-56 bg-black  my-1">
+          <p className="mx-3 text-normal text-center mt-8 ">
+          Active Courses {" "}
+          </p>
+          <p className="text-center lg:text-7xl md:text-7xl text-6xl my-6">36</p>
+          <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-2.5 " onClick={redirectcourse}>
+            View
+          </button>
+        </div>
+        <div className="lg:w-52 md:w-48 w-36 h-56 bg-black  my-1">
+          <p className="mx-3 text-normal text-center mt-8 ">
+          Completed Courses{" "}
+          </p>
+          <p className="text-center lg:text-7xl md:text-7xl text-6xl my-6">37</p>
+          <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-2.5  " onClick={redirectcourse}>
+            View
+          </button>
+        </div>
+      </div>
+      <p className="text-lg my-4">Recent Courses</p>
+      <div className="flex gap-4 flex-wrap">
+        {course.map((data,index)=>(
+        <div className="lg:w-52 md:w-52 w-48 h-60 bg-black my-1" key={index}>
+            <img src={data.images} alt="Image" className="w-full"/>
+            <p className="text-lg mx-2 my-0.5">{data.topic}</p>
+            <p className="text-sm mx-2 my-0.5">{data.type}</p>
+            <p className="text-sm mx-2 mb-3">{data.type}</p>
+            <button className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-full py-1.5 " onClick={redirectcourse}>
+            Continue
+          </button>
+        </div>
+        ))}
+        
+      </div>
     </div>
-   </div>
   );
 };
 
--- src/pages/layout/Headers.jsx
+++ src/pages/layout/Headers.jsx
@@ -1,27 +1,42 @@
-import React, { useState } from 'react';
-import PMCLogo from '../../assets/PMC_Logo.png';
+import React, { useState } from "react";
+import PMCLogo from "../../assets/PMC_Logo.png";
 import { IoNotifications } from "react-icons/io5";
 import profile from "../../assets/profile.png";
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const Headers = ({ Menus }) => {
+  const navigate = useNavigate();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [notify, setNotify] = useState(false);
   const location = useLocation();
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const togglenotify = () => {
+    setNotify(!notify);
+  };
+
+  const redirectnotify = () => {
+    setNotify(!notify);
+    navigate('/notify')
+  }
+
+
   return (
-    <div className='flex justify-between items-center px-3 bg-gradient-to-r from-[#110038] to-[#08006B] z-50 fixed w-full'>
-      <img src={PMCLogo} alt="PickMyCourse" className='w-42 h-14' />
-      
-      <p className='text-2xl text-white md:block lg:hidden block' onClick={toggleDropdown}>
+    <div className="flex justify-between items-center px-3 bg-gradient-to-r from-[#110038] to-[#08006B] z-50 fixed w-full">
+      <img src={PMCLogo} alt="PickMyCourse" className="w-42 h-14" />
+
+      <p
+        className="text-2xl text-white md:block lg:hidden block"
+        onClick={toggleDropdown}
+      >
         Menu
       </p>
 
       {isDropdownOpen && (
-        <div className='absolute right-0 top-14 w-full bg-[#200098] text-white shadow-md lg:hidden md:block block'>
+        <div className="absolute right-0 top-14 w-full bg-[#200098] text-white shadow-md lg:hidden md:block block">
           <div className="flex gap-2 items-center pt-3 mx-3">
             <img src={profile} alt="User" className="w-14 h-14" />
             <div>
@@ -34,7 +49,7 @@ const Headers = ({ Menus }) => {
           </div>
           <ul className="flex flex-wrap py-5 ">
             {Menus.map((menu, index) => (
-              <NavLink to={menu.to} onClick={menu.onClick} key={index} >
+              <NavLink to={menu.to} onClick={menu.onClick} key={index}>
                 <li
                   className={`cursor-pointer text-md flex items-center gap-x-3 p-2 mt-1 pl-3 transition-all duration-700 ${
                     location.pathname === menu.to
@@ -56,9 +71,33 @@ const Headers = ({ Menus }) => {
         </div>
       )}
 
-      <div className="relative">
-        <IoNotifications className='lg:w-7 lg:h-7 md:w-6 md:h-6 w-5 h-5 text-white cursor-pointer' />
+      <div className="">
+        <IoNotifications
+          className="lg:w-7 lg:h-7 md:w-6 md:h-6 w-5 h-5 text-white cursor-pointer"
+          onClick={togglenotify}
+        />
       </div>
+      {notify && (
+        <div className="absolute text-black right-2 top-10 bg-white w-80 h-fit pt-3 font-poppins font-extralight">
+          <p className="px-2">Notifications</p>
+          <hr />
+          <span className=" flex gap-1 justify-between px-3 py-2">
+            <p className="text-normal text-slate-600">
+              Subject: Lhoereemmis apilsiudmenistered a dummy Text
+            </p>
+            <button className="text-sm text-black font-normal " onClick={redirectnotify}>View</button>
+          </span>
+          <hr />
+          <span className=" flex gap-1 justify-between px-3 py-2">
+            <p className="text-normal text-slate-600">
+              Subject: Lhoereemmis apilsiudmenistered a dummy Text
+            </p>
+            <button className="text-sm text-black font-normal " onClick={redirectnotify}>View</button>
+          </span>
+
+          <button className="w-full bg-slate-400 text-lg py-1" onClick={redirectnotify}>See all</button>
+        </div>
+      )}
     </div>
   );
 };
