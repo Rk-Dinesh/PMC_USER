dc3b435d785f29eed2a50e396b4dd8715bd5e66e
Rk-Dinesh <rdkumar881@gmail.com>
2024-11-18 10:59:41 +0530

    commit

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Logo from "../../assets/PMC_Logo.png";
 import { useNavigate } from "react-router-dom";
 import cover from "../../assets/bgimage.png";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -13,6 +15,8 @@ const SignIn = () => {
   const redirectOtp = () => {
     navigate("/otp");
   };
+
+  
 
   return (
     <div className="bg-[#300080] h-screen flex justify-center items-center font-poppins text-white  ">
@@ -26,15 +30,25 @@ const SignIn = () => {
           <img src={Logo} alt="Logo" className="w-full " />
           <p className="text-center text-lg my-2">Login</p>
           <div className="flex flex-col gap-3 mx-2 my-4 ">
-            <label htmlFor="phone">
-              Phone <span className="text-red-600">*</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Phone"
-              required
-              className="py-2  rounded-md text-center text-black shadow-md outline-none"
+           <span className="">
+           <PhoneInput
+              country={"in"}
+              // value={this.state.phone}
+              // onChange={phone => this.setState({ phone })}
+              className="text-black mx-8 "
+              // containerStyle={{ border: '1px solid #ccc', borderRadius: '5px' }}
+          inputStyle={{ padding: '10px',color:'black',marginLeft:'38px' }}
+          buttonStyle={{
+            background: 'linear-gradient(to right, #3D03FA, #A71CD2)', // Gradient background
+            border: 'none', // No border
+            paddingRight:'10px',
+            
+          }}
+          dropdownStyle={{ backgroundColor: '#fff', border: '1px solid #ccc' }}
+          // searchStyle={{ padding: '5px', fontSize: '14px' }}
+              
             />
+           </span>
             <div className="flex justify-center my-8">
               <button
                 className=" text-lg bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] w-1/2 py-2.5 "
@@ -44,6 +58,7 @@ const SignIn = () => {
               </button>
             </div>
           </div>
+
           <p className="text-center text-lg py-2" onClick={redirectSignUp}>
             Don't have an account?
           </p>
