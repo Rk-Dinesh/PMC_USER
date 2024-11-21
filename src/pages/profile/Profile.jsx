22520ec23ef728f03eada43426ffdda17366f08a
Rk-Dinesh <rdkumar881@gmail.com>
2024-11-21 16:04:02 +0530

    update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Profiles from "../../assets/profile.png";
 import UpdateProfile from "./UpdateProfile";
 import UpdatePhone from "./UpdatePhone";
 import UpdateImage from "./UpdateImage";
+import axios from "axios";
+import { API } from "../../Host";
 
 const Profile = () => {
   const [isModal, setIsModal] = useState(false);
   const [isPhoneModal, setIsPhoneModal] = useState(false);
   const [isProfileModal, setIsProfileModal] = useState(false);
+  const user = localStorage.getItem("user");
+
+  const [userData, setUserData] = useState({});
+  const [userImage, setUserImage] = useState({});
+
+  useEffect(() => {
+    fetchUser();
+    fetchImage();
+  }, [isProfileModal]);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(`${API}/api/getusersbyid/${user}`);
+      const responseData = response.data.user;
+      setUserData(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchImage = async () => {
+    try {
+      const response = await axios.get(`${API}/api/getimagebyid?user=${user}`);
+      const responseData = response.data.user;
+      setUserImage(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const CloseEmailModal = () => {
     setIsModal(!isModal);
@@ -25,9 +56,13 @@ const Profile = () => {
     <>
       <div className="mx-5 my-6 font-poppins">
         <div className=" flex flex-col mx-6">
-          <img src={Profiles} alt="Image" className="w-40 h-40" />
+          <img
+            src={userImage?.image ? userImage.image : Profiles}
+            alt="Profile"
+            className={`w-40 h-40 ${userImage?.image ? ' rounded-3xl object-cover' : ''}`}
+          />
           <button
-            className={` text-base  bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] lg:w-40 md:w-40 w-40 py-2.5 my-3 `}
+            className={` text-base  bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] lg:w-40 md:w-40 w-40 py-2.5 my-5 `}
             onClick={() => setIsProfileModal(true)}
           >
             Change Image
@@ -41,24 +76,27 @@ const Profile = () => {
               <label htmlFor="fname">First Name</label>
               <input
                 type="text"
-                className=" py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
+                className=" text-white py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
                 placeholder="John"
+                value={userData?.fname || ''}
               />
             </div>
             <div className="lg:col-span-4 md:col-span-4 col-span-12 ">
               <label htmlFor="lname">Last Name</label>
               <input
                 type="text"
-                className=" py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
+                className="text-white py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
                 placeholder="Doe"
+                value={userData?.lname || ''}
               />
             </div>
             <div className="lg:col-span-4 md:col-span-4 col-span-12 ">
               <label htmlFor="dob">Date Of Birth</label>
               <input
                 type="text"
-                className=" py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
+                className="text-white py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-gray-400 outline-none  disabled:pointer-events-none"
                 placeholder="25-12-2024"
+                value={userData?.dob || ''}
               />
             </div>
           </div>
@@ -74,6 +112,7 @@ const Profile = () => {
                   type="text"
                   className=" py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-white outline-none  disabled:pointer-events-none text-white"
                   placeholder="johndoe@gmail.com"
+                  value={userData?.email || ''}
                 />
               </div>
               <div className="flex justify-center my-2">
@@ -92,6 +131,7 @@ const Profile = () => {
                   type="text"
                   className=" py-3 pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b border-x-transparent border-b-white outline-none  disabled:pointer-events-none text-white"
                   placeholder="1234567890"
+                  value={userData?.phone || ''}
                 />
               </div>
               <div className="flex justify-center my-2">
--- src/pages/profile/UpdateImage.jsx
+++ src/pages/profile/UpdateImage.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Modal from "../../components/Modal";
+import axios from "axios";
+import { API } from "../../Host";
+import { toast } from "react-toastify";
 
 const UpdateImage = ({ CloseProfileModal }) => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -14,7 +17,7 @@ const UpdateImage = ({ CloseProfileModal }) => {
     }
     const maxSizeInBytes = 2 * 1024 * 1024; // 2MB
     if (file.size > maxSizeInBytes) {
-      alert("File size exceeds 1MB!");
+      toast.error("File size exceeds 2MB!");
       return;
     }
     setSelectedFile(file);
@@ -29,18 +32,27 @@ const UpdateImage = ({ CloseProfileModal }) => {
 
   const handleUpload = async () => {
     if (!selectedFile) {
-      alert("No file selected!");
+      toast.error("No file selected!");
       return;
     }
 
     const payload = {
       name: selectedFile.name,
+      user: localStorage.getItem("user"),
       image: base64Image,
     };
 
-    console.log(payload);
-
-
+    try {
+      const response = await axios.post(`${API}/api/images`, payload);
+      if (response.status === 200) {
+        toast.success("Profile Image Updated Succesfully");
+        CloseProfileModal();
+      } else {
+        console.log("Failed to Upload");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <Modal>
@@ -54,41 +66,42 @@ const UpdateImage = ({ CloseProfileModal }) => {
         <p className="mx-16 text-xl my-2">Choose Profile Image</p>
 
         <div className="relative mt-6 mx-16 ">
-                <label className="block bg-white rounded-lg">
-                  <span className="sr-only">Choose File</span>
-                  <input type="file" className="hidden" id="file-input" onChange={handleFileChange}  />
-                  <button
-                    className="bg-gray-300 text-black px-4 py-2.5 rounded-md"
-                    for="file-input"
-                  >
-                    Choose Files
-                  </button>
-                </label>
-                <span
-                  className="absolute top-1/2 -translate-y-1/2 lg:right-4 md:right-4 right-16 text-normal text-black"
-                  id="file-name"
-                >
-                  No Files Chosen
-                </span>
-              </div>
-        {preview && (
-        <div className="text-center text-lg my-2">
-            <p className="text-center text-xl my-2">Preview </p>
-          <img
-            src={preview}
-            alt="Preview"
-           className="w-60 mx-auto"
-          />
-        </div>
-      )}
-       <div className="flex justify-center my-8">
+          <label className="block bg-white rounded-lg">
+            <span className="sr-only">Choose File</span>
+            <input
+              type="file"
+              className="hidden"
+              id="file-input"
+              onChange={handleFileChange}
+            />
             <button
-              className={` font-normal   bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] text-center w-36 py-2.5 my-2 `}
-              onClick={handleUpload}
+              className="bg-gray-300 text-black px-4 py-2.5 rounded-md"
+              for="file-input"
             >
-              Upload Image
+              Choose Files
             </button>
+          </label>
+          <span
+            className="absolute top-1/2 -translate-y-1/2 lg:right-4 md:right-4 right-16 text-normal text-black"
+            id="file-name"
+          >
+            No Files Chosen
+          </span>
+        </div>
+        {preview && (
+          <div className="text-center text-lg my-2">
+            <p className="text-center text-xl my-2">Preview </p>
+            <img src={preview} alt="Preview" className="w-60 mx-auto" />
           </div>
+        )}
+        <div className="flex justify-center my-8">
+          <button
+            className={` font-normal   bg-gradient-to-r from-[#3D03FA] to-[#A71CD2] text-center w-36 py-2.5 my-2 `}
+            onClick={handleUpload}
+          >
+            Upload Image
+          </button>
+        </div>
       </div>
     </Modal>
   );
